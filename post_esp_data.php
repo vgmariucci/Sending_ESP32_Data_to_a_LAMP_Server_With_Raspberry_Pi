807f131af5947ae5fde3aada9af8537593c272ab
vgmariucci <vgmariucci@hotmail.com>
2024-11-06 00:19:55 -0300

    typo correnctions in post_esp_data.php and  esp_data.php

--- post_esp_data.php
+++ post_esp_data.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-mysql-database-php/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+
 require 'vendor/autoload.php'; // Load Composer dependencies
 
 // Load the .env file
