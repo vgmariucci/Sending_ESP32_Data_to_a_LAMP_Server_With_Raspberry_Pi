29d2639452a50a947418c9a93eb670f8f9c3e403
vgmariucci <vgmariucci@hotmail.com>
2025-01-22 23:52:56 -0300

    Inclusion of iot_device_serial_number and customer_ID into Secrets_Example.h file

--- arduino_files/mainScript/Secrets_Example.h
+++ arduino_files/mainScript/Secrets_Example.h
@@ -1,8 +1,14 @@
 
 // Replace with the URL path or Raspberry Pi IP address and the php script responsible to receive ESP32 data
-const char serverName[] = "http://your_raspberrypi_IP_address/post_esp_data.php";
-
+const char serverName[] = "http://your_raspberrypi_IP_address//post_esp_data.php";
 
 // Keep this API Key value to be compatible with the PHP code provided in the project page. 
 // If you change the apiKeyValue value, the PHP file /post_esp_data.php also needs to have the same key 
 String API_KEY = "change_me_for_your_api_key";
+
+      
+// IoT device serial number
+String  iot_device_serial_number = "device_serial_number";
+
+// Customer identification
+String customer_ID = "Customer_ID";
